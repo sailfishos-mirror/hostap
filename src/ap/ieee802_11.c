sailfishos-mirror/hostap

commit 7907603de14d74a56c9ab96b31e78baeade54d7e

RNR: Fix RNR issues in legacy Beacon frames
With the per-BSS configuration option disable_11be=1, it is possible to
have a non-MLO/non-EHT BSS under an MLO capable interface/radio. There
is no restriction to further add MLO capable BSSs to the same interface
where a legacy BSS is also added. So, it is allowed to have both legacy
and MLO BSSs under the same interface.

When a lower band (2.4/5 GHz) interface/radio has a co-located 6 GHz
interface/radio under the same platform, the BSSs affiliated with lower
band interface have to include the RNR information of each co-located 6
GHz BSS.

RNR of a BSS that is reported in Management frames can be of two types
- normal RNR (13 bytes)
- ML RNR (13 bytes + 3 bytes for MLD parameters).

Reporting AP should include normal RNR for a co-located 6 GHz BSS when
 - the reporting AP itself is not MLO capable (despite of co-located
   BSSs having MLO capability).
 - the reporting AP is MLO capable, but the co-located BSS is not MLO
   capable.

ML RNR is to be included only when both reporting AP and co-located BSS
are MLO capable. Otherwise, normal RNR is to be reported as mentioned
above.

Current code has a bug where a non-MLO lower band interface's BSS
reports ML RNR for a MLO capable co-located 6 GHz BSS. This cannot be
decoded by non-MLD stations. To fix this
- Check for both reporting BSS's and reported BSS's MLO capability in
  hostapd_skip_rnr() to skip normal RNR.
- After calculating the length of normal RNR TBTTs, do not repeat the
calculation which tries to add the length of ML RNR TBTTs as well if the
reporting AP itself is not MLO capable. Expectation is, normal RNR
should be added for all neighbor 6 GHz BSSs.
- After adding normal RNR TBTTs for co-located 6 GHz BSSs, do not repeat
to add ML RNR TBTTs if the reporting AP itself is not MLO capable.
Expectation is, normal RNR TBTTs should be added for all neighbor 6 GHz
BSS.

Fixes: de1bfda64ef7 ("Fix RNR building for co-location and MLO")

src/ap/ieee802_11.c
@@ -8140,9 +8140,11 @@ static bool hostapd_skip_rnr(size_t i, struct mbssid_ie_profiles *skip_profiles,
 	    i >= skip_profiles->start && i < skip_profiles->end)
 		return true;
 
-	/* No need to report if length is for normal TBTT and the BSS is
-	 * affiliated with an AP MLD. MLD TBTT will include this. */
-	if (tbtt_info_len == RNR_TBTT_INFO_LEN && ap_mld)
+	/* No need to report if length is for normal TBTT and both the reporting
+	 * AP and neighbor AP are affiliated with an AP MLD. MLD TBTT will
+	 * include this. */
+	if (tbtt_info_len == RNR_TBTT_INFO_LEN && ap_mld &&
+	    reporting_hapd->conf->mld_ap)
 		return true;
 
 	/* No need to report if length is for MLD TBTT and the BSS is not
@@ -8241,9 +8243,10 @@ repeat_rnr_len:
 	total_tbtt_count += tbtt_count;
 
 	/* If building for co-location, re-build again but this time include
-	 * ML TBTTs.
+	 * ML TBTTs if the reporting AP is affiliated with an AP MLD.
 	 */
-	if (!mld_update && tbtt_info_len == RNR_TBTT_INFO_LEN) {
+	if (!mld_update && tbtt_info_len == RNR_TBTT_INFO_LEN &&
+	    reporting_hapd->conf->mld_ap) {
 		tbtt_info_len = RNR_TBTT_INFO_MLD_LEN;
 
 		/* If no TBTT was found, adjust the len and total_len since it
@@ -8536,7 +8539,8 @@ static bool hostapd_eid_rnr_bss(struct hostapd_data *hapd,
 	*eid++ = RNR_20_MHZ_PSD_MAX_TXPOWER;
 
 #ifdef CONFIG_IEEE80211BE
-	if (ap_mld) {
+	/* Include the MLD parameters only when TBTT length is for ML RNR */
+	if (ap_mld && tbtt_info_len == RNR_TBTT_INFO_MLD_LEN) {
 		u8 param_ch = bss->eht_mld_bss_param_change;
 
 		/* If BSS is not a partner of the reporting_hapd or
@@ -8628,9 +8632,10 @@ repeat_rnr:
 	total_tbtt_count += tbtt_count;
 
 	/* If building for co-location, re-build again but this time include
-	 * ML TBTTs.
+	 * ML TBTTs if the reporting AP is affiliated with an AP MLD.
 	 */
-	if (!mld_update && tbtt_info_len == RNR_TBTT_INFO_LEN) {
+	if (!mld_update && tbtt_info_len == RNR_TBTT_INFO_LEN &&
+	    reporting_hapd->conf->mld_ap) {
 		tbtt_info_len = RNR_TBTT_INFO_MLD_LEN;
 		goto repeat_rnr;
 	}

commit 9a771d4d7916b799f3aa3afdfd595e04ab2741a4

AP MLD: Fix the compilation errors without CONFIG_IEEE80211BE
Fix the compilation errors by protecting the access to mld_ap in
RNR handlers for the CONFIG_IEEE80211BE disabled case.

Fixes: 7907603de14d ("RNR: Fix RNR issues in legacy Beacon frames")
Signed-off-by: Shwetha G K <kshwetha@qti.qualcomm.com>

src/ap/ieee802_11.c
@@ -8146,6 +8146,12 @@ static bool hostapd_skip_rnr(size_t i, struct mbssid_ie_profiles *skip_profiles,
 			     struct hostapd_data *reporting_hapd,
 			     struct hostapd_data *bss, u8 *match_idx)
 {
+	bool reporting_ap_mld = false;
+
+#ifdef CONFIG_IEEE80211BE
+	reporting_ap_mld = !!reporting_hapd->conf->mld_ap;
+#endif /* CONFIG_IEEE80211BE */
+
 	if (!mld_update && skip_profiles &&
 	    i >= skip_profiles->start && i < skip_profiles->end)
 		return true;
@@ -8153,8 +8159,7 @@ static bool hostapd_skip_rnr(size_t i, struct mbssid_ie_profiles *skip_profiles,
 	/* No need to report if length is for normal TBTT and both the reporting
 	 * AP and neighbor AP are affiliated with an AP MLD. MLD TBTT will
 	 * include this. */
-	if (tbtt_info_len == RNR_TBTT_INFO_LEN && ap_mld &&
-	    reporting_hapd->conf->mld_ap)
+	if (tbtt_info_len == RNR_TBTT_INFO_LEN && ap_mld && reporting_ap_mld)
 		return true;
 
 	/* No need to report if length is for MLD TBTT and the BSS is not
@@ -8200,6 +8205,11 @@ hostapd_eid_rnr_iface_len(struct hostapd_data *hapd,
 	size_t i, start;
 	u8 tbtt_info_len = mld_update ? RNR_TBTT_INFO_MLD_LEN :
 		RNR_TBTT_INFO_LEN;
+	bool reporting_ap_mld = false;
+
+#ifdef CONFIG_IEEE80211BE
+	reporting_ap_mld = !!reporting_hapd->conf->mld_ap;
+#endif /* CONFIG_IEEE80211BE */
 
 repeat_rnr_len:
 	start = 0;
@@ -8256,7 +8266,7 @@ repeat_rnr_len:
 	 * ML TBTTs if the reporting AP is affiliated with an AP MLD.
 	 */
 	if (!mld_update && tbtt_info_len == RNR_TBTT_INFO_LEN &&
-	    reporting_hapd->conf->mld_ap) {
+	    reporting_ap_mld) {
 		tbtt_info_len = RNR_TBTT_INFO_MLD_LEN;
 
 		/* If no TBTT was found, adjust the len and total_len since it
@@ -8599,6 +8609,7 @@ static u8 * hostapd_eid_rnr_iface(struct hostapd_data *hapd,
 	u8 tbtt_count, total_tbtt_count = 0, op_class, channel;
 	u8 tbtt_info_len = mld_update ? RNR_TBTT_INFO_MLD_LEN :
 		RNR_TBTT_INFO_LEN;
+	bool reporting_ap_mld = false;
 
 	if (!(iface->drv_flags & WPA_DRIVER_FLAGS_AP_CSA) || !iface->freq)
 		return eid;
@@ -8609,6 +8620,10 @@ static u8 * hostapd_eid_rnr_iface(struct hostapd_data *hapd,
 					  &op_class, &channel) ==
 	    NUM_HOSTAPD_MODES)
 		return eid;
+
+#ifdef CONFIG_IEEE80211BE
+	reporting_ap_mld = !!reporting_hapd->conf->mld_ap;
+#endif /* CONFIG_IEEE80211BE */
 
 repeat_rnr:
 	start = 0;
@@ -8647,7 +8662,7 @@ repeat_rnr:
 	 * ML TBTTs if the reporting AP is affiliated with an AP MLD.
 	 */
 	if (!mld_update && tbtt_info_len == RNR_TBTT_INFO_LEN &&
-	    reporting_hapd->conf->mld_ap) {
+	    reporting_ap_mld) {
 		tbtt_info_len = RNR_TBTT_INFO_MLD_LEN;
 		goto repeat_rnr;
 	}