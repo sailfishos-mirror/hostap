b18dd2add0129fbc735a12139ac0732f6c29f72c
Arowa Suliman <arowa@chromium.org>
2026-03-03 20:29:32 +0000

    SME: Omit RSNXE for WPA2-Personal connections
    The RSNXE was added after Wi-Fi Alliance launched WPA2. While in theory
    it applies to WPA2 as far as the IEEE 802.11 standard is concerned,
    including it in WPA2 handshakes can cause authentication failures with
    some misbehaving APs, as they do not handle its presence correctly in a
    WPA2 context.
    
    It feels unlikely that WPA2 APs would be extended to support features
    that are negotiated using the RSNXE, so do not include the RSNXE for a
    WPA2-Personal connection from wpa_supplicant SME to work around
    interoperability issues. This is similar to what was already done for
    WPA(v1) in commit f531b6d2092a ("SME: Do not send RSNXE for WPA 1
    connections").
    
    Known impacted routers: TP-Link EAP320 and TP-Link Archer VR400
    Test: PASN enabled connect Intel WiFi AX211 to WPA2-PSK network with TP-Link EAP320 router
    
    Signed-off-by: Arowa Suliman <arowa@chromium.org>

--- wpa_supplicant/sme.c
+++ wpa_supplicant/sme.c
@@ -1065,6 +1065,11 @@ static void sme_send_authentication(struct wpa_supplicant *wpa_s,
 			wpas_connect_work_done(wpa_s);
 			return;
 		}
+	} else if (wpa_s->wpa_proto == WPA_PROTO_RSN &&
+		   wpa_key_mgmt_wpa_psk_no_sae(wpa_s->key_mgmt)) {
+		/* Omit RSNXE for WPA2-Personal connections to avoid AP
+		 * compatibility issues. */
+		omit_rsnxe = 1;
 	}
 
 #ifdef CONFIG_IEEE80211R
