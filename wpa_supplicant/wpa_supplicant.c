sailfishos-mirror/hostap

commit 19a3b556722e590e6d695c9c9eaa3909c09e0f99

wpa_supplicant: Always enable Data frame filtering
Always enable IP unicast in L2/Wi-Fi multicast filtering for all
networks (not just Passpoint non-DGAF ones) by default, but provide an
option to turn it off in case someone finds it breaks something.

Also provide an option (but off by default) to always (and not just in
Passpoint networks) rely on proxy ARP/NS if the AP advertises it and in
that case drop gratuitous ARP (IPv4) and unsolicited NA (IPv6) packets.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

wpa_supplicant/wpa_supplicant.c
@@ -797,10 +797,8 @@ static void wpa_supplicant_cleanup(struct wpa_supplicant *wpa_s)
 	wpa_s->p2p_pmksa_entry = NULL;
 #endif /* CONFIG_P2P */
 
-#ifdef CONFIG_HS20
 	if (wpa_s->drv_priv)
 		wpa_drv_configure_frame_filters(wpa_s, 0);
-#endif /* CONFIG_HS20 */
 
 	for (i = 0; i < NUM_VENDOR_ELEM_FRAMES; i++) {
 		wpabuf_free(wpa_s->vendor_elem[i]);
@@ -4053,11 +4051,11 @@ static u8 * wpas_populate_assoc_ies(
 				wpa_ie_len += wpabuf_len(hs20);
 			}
 			wpabuf_free(hs20);
-
-			hs20_configure_frame_filters(wpa_s);
 		}
 	}
 #endif /* CONFIG_HS20 */
+
+	wpas_configure_frame_filters(wpa_s);
 
 	if (wpa_s->vendor_elem[VENDOR_ELEM_ASSOC_REQ]) {
 		struct wpabuf *buf = wpa_s->vendor_elem[VENDOR_ELEM_ASSOC_REQ];
@@ -5048,9 +5046,7 @@ static void wpas_start_assoc_cb(struct wpa_radio_work *work, int deinit)
 #endif /* CONFIG_P2P */
 	    ssid->bssid_set) {
 		wpa_s->current_bss = bss;
-#ifdef CONFIG_HS20
-		hs20_configure_frame_filters(wpa_s);
-#endif /* CONFIG_HS20 */
+		wpas_configure_frame_filters(wpa_s);
 	}
 
 	wpa_supplicant_rsn_supp_set_config(wpa_s, wpa_s->current_ssid);
@@ -10223,4 +10219,54 @@ void wpas_update_dfs_ap_info(struct wpa_supplicant *wpa_s, int freq,
 		p2p_update_dfs_ap_info(wpa_s->global->p2p, freq, ap_ch_width,
 				       disconnect_evt);
 #endif /* CONFIG_P2P */
+}
+
+
+void wpas_configure_frame_filters(struct wpa_supplicant *wpa_s)
+{
+	struct wpa_bss *bss = wpa_s->current_bss;
+	u32 filter = 0;
+	bool hs20, proxy_arp_capa;
+
+	if (!bss)
+		return;
+
+#ifdef CONFIG_HS20
+	hs20 = is_hs20_network(wpa_s, wpa_s->current_ssid, bss);
+#else /* CONFIG_HS20 */
+	hs20 = false;
+#endif /* CONFIG_HS20 */
+
+	if (wpa_s->current_ssid &&
+	    wpa_s->current_ssid->drop_unicast_ip_in_l2_multicast) {
+		filter |= WPA_DATA_FRAME_FILTER_FLAG_GTK;
+	} else if (!hs20) {
+		/* Not configuring frame filtering - BSS is not a Hotspot 2.0
+		 * network */
+		return;
+	} else {
+#ifdef CONFIG_HS20
+		const u8 *ie;
+
+		ie = wpa_bss_get_vendor_ie(bss, HS20_IE_VENDOR_TYPE);
+
+		/* Check if DGAF disabled bit is zero (5th byte in the IE) */
+		if (!ie || ie[1] < 5)
+			wpa_printf(MSG_DEBUG,
+				   "Not configuring frame filtering - Can't extract DGAF bit");
+		else if (!(ie[6] & HS20_DGAF_DISABLED))
+			filter |= WPA_DATA_FRAME_FILTER_FLAG_GTK;
+#endif /* CONFIG_HS20 */
+	}
+
+	proxy_arp_capa = wpa_bss_ext_capab(bss, WLAN_EXT_CAPAB_PROXY_ARP);
+
+	if ((hs20 && proxy_arp_capa) ||
+	    wpa_s->current_ssid->always_use_proxy_arp == 2 ||
+	    (proxy_arp_capa && wpa_s->current_ssid &&
+	     wpa_s->current_ssid->always_use_proxy_arp == 1))
+		filter |= WPA_DATA_FRAME_FILTER_FLAG_ARP |
+			WPA_DATA_FRAME_FILTER_FLAG_NA;
+
+	wpa_drv_configure_frame_filters(wpa_s, filter);
 }

wpa_supplicant/config.c
@@ -2843,6 +2843,8 @@ static const struct parse_data ssid_fields[] = {
 #ifdef CONFIG_PMKSA_PRIVACY
 	{ INT_RANGE(pmksa_privacy, 0, 1)},
 #endif /* CONFIG_PMKSA_PRIVACY */
+	{ INT_RANGE(drop_unicast_ip_in_l2_multicast, 0, 1)},
+	{ INT_RANGE(always_use_proxy_arp, 0, 2)},
 };
 
 #undef OFFSET
@@ -3408,6 +3410,7 @@ void wpa_config_set_network_defaults(struct wpa_ssid *ssid)
 	ssid->mac_addr = WPAS_MAC_ADDR_STYLE_NOT_SET;
 	ssid->max_oper_chwidth = DEFAULT_MAX_OPER_CHWIDTH;
 	ssid->rsn_overriding = RSN_OVERRIDING_NOT_SET;
+	ssid->drop_unicast_ip_in_l2_multicast = true;
 }
 
 

wpa_supplicant/config_file.c
@@ -1040,6 +1040,8 @@ static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid,
 #ifdef CONFIG_PMKSA_PRIVACY
 	INT(pmksa_privacy);
 #endif /* CONFIG_PMKSA_PRIVACY */
+	INT_DEF(drop_unicast_ip_in_l2_multicast, 1);
+	INT_DEF(always_use_proxy_arp, 0);
 #undef STR
 #undef INT
 #undef INT_DEF

wpa_supplicant/config_ssid.h
@@ -1354,6 +1354,22 @@ struct wpa_ssid {
 	 * connection even if the same cached PMKSA is reused.
 	 */
 	int pmksa_privacy;
+
+	/**
+	 * drop_unicast_ip_in_l2_multicast - Drop unicast IP packets in L2
+	 *	multicast frames in all networks, not just Passpoint non-DGAF
+	 *	networks
+	 */
+	bool drop_unicast_ip_in_l2_multicast;
+
+	/**
+	 * always_use_proxy_arp - Always rely on proxy ARP even in non-Passpoint
+	 *	networks
+	 * 0 = disabled
+	 * 1 = check AP's extended capabilities
+	 * 2 = always enable
+	 */
+	u8 always_use_proxy_arp;
 };
 
 #endif /* CONFIG_SSID_H */

wpa_supplicant/hs20_supplicant.c
@@ -29,43 +29,6 @@
 #include "notify.h"
 
 
-void hs20_configure_frame_filters(struct wpa_supplicant *wpa_s)
-{
-	struct wpa_bss *bss = wpa_s->current_bss;
-	const u8 *ie;
-	const u8 *ext_capa;
-	u32 filter = 0;
-
-	if (!bss || !is_hs20_network(wpa_s, wpa_s->current_ssid, bss)) {
-		/* Not configuring frame filtering - BSS is not a Hotspot 2.0
-		 * network */
-		return;
-	}
-
-	ie = wpa_bss_get_vendor_ie(bss, HS20_IE_VENDOR_TYPE);
-
-	/* Check if DGAF disabled bit is zero (5th byte in the IE) */
-	if (!ie || ie[1] < 5)
-		wpa_printf(MSG_DEBUG,
-			   "Not configuring frame filtering - Can't extract DGAF bit");
-	else if (!(ie[6] & HS20_DGAF_DISABLED))
-		filter |= WPA_DATA_FRAME_FILTER_FLAG_GTK;
-
-	ext_capa = wpa_bss_get_ie(bss, WLAN_EID_EXT_CAPAB);
-	if (!ext_capa || ext_capa[1] < 2) {
-		wpa_printf(MSG_DEBUG,
-			   "Not configuring frame filtering - Can't extract Proxy ARP bit");
-		return;
-	}
-
-	if (wpa_bss_ext_capab(bss, WLAN_EXT_CAPAB_PROXY_ARP))
-		filter |= WPA_DATA_FRAME_FILTER_FLAG_ARP |
-			WPA_DATA_FRAME_FILTER_FLAG_NA;
-
-	wpa_drv_configure_frame_filters(wpa_s, filter);
-}
-
-
 void wpas_hs20_add_indication(struct wpabuf *buf, int pps_mo_id, int ap_release)
 {
 	int release;

wpa_supplicant/hs20_supplicant.h
@@ -8,7 +8,6 @@
 #ifndef HS20_SUPPLICANT_H
 #define HS20_SUPPLICANT_H
 
-void hs20_configure_frame_filters(struct wpa_supplicant *wpa_s);
 void wpas_hs20_add_indication(struct wpabuf *buf, int pps_mo_id,
 			      int ap_release);
 void wpas_hs20_add_roam_cons_sel(struct wpabuf *buf,

wpa_supplicant/sme.c
@@ -1440,9 +1440,7 @@ no_fils:
 	if (old_ssid != wpa_s->current_ssid)
 		wpas_notify_network_changed(wpa_s);
 
-#ifdef CONFIG_HS20
-	hs20_configure_frame_filters(wpa_s);
-#endif /* CONFIG_HS20 */
+	wpas_configure_frame_filters(wpa_s);
 
 #ifdef CONFIG_P2P
 	/*

wpa_supplicant/wpa_supplicant.conf
@@ -1795,6 +1795,22 @@ fast_reauth=1
 # 2 = Supports Multi-AP profile 2 as defined in Wi-Fi EasyMesh specification
 #multi_ap_profile=2
 
+# Unicast IP packet in L2 multicast frame filtering
+# Allow turning off IP unicast in L2 multicast frame filtering, which is enabled
+# by default.
+#drop_unicast_ip_in_l2_multicast=1
+
+# Proxy ARP/NS reliance
+# Enable Proxy ARP/NS reliance, dropping gratuitous ARP and unsolicited NA
+# frames when the AP supports proxy ARP even on networks that aren't Passpoint.
+# If set to 0 (the default), this is only done on Passpoint networks.
+# If set to 1, the AP's extended capabilities are checked, and proxy ARP/NS
+# is used when available.
+# If set to 2, the feature is enabled regardless of the AP's extended
+# capabilities, to allow forcing it in networks known to have it, since
+# the extended capabilities are not cryptographically checked.
+#always_use_proxy_arp=0
+
 ##### Fast Session Transfer (FST) support #####################################
 #
 # The options in this section are only available when the build configuration

commit 71b6342289c9cd8439cc93d9911527aefc7ca9bc

Check wpa_s->current_ssid more consistently
One of the cases in wpas_configure_frame_filters() was not checking
whether this pointer is set before dereferencing it.

Fixes: 19a3b556722e ("wpa_supplicant: Always enable Data frame filtering")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/wpa_supplicant.c
@@ -10262,7 +10262,8 @@ void wpas_configure_frame_filters(struct wpa_supplicant *wpa_s)
 	proxy_arp_capa = wpa_bss_ext_capab(bss, WLAN_EXT_CAPAB_PROXY_ARP);
 
 	if ((hs20 && proxy_arp_capa) ||
-	    wpa_s->current_ssid->always_use_proxy_arp == 2 ||
+	    (wpa_s->current_ssid &&
+	     wpa_s->current_ssid->always_use_proxy_arp == 2) ||
 	    (proxy_arp_capa && wpa_s->current_ssid &&
 	     wpa_s->current_ssid->always_use_proxy_arp == 1))
 		filter |= WPA_DATA_FRAME_FILTER_FLAG_ARP |