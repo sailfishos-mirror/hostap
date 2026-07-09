greearb/hostap-ct

commit ef228583e33187d0384ce51453c1a2b8ca3afc24

SAE: Password identifier changing (STA)
Add support for changing the SAE password identifier value that is sent
in SAE commit messages for privacy protection in cases where random MAC
addresses are used with per-STA (or per-user) SAE password identifiers.
At least for now, this functionality is disabled by default and needs to
be enabled with sae_password_id_change=1 in a network profile that uses
an SAE password identifier (sae_password_id=..). This mechanism might
get enabled by default in the future once the protocol specification
becomes more mature and there has been interoperability testing between
different implementations.

The implemented functionality is for the definition that were added in
IEEE P802.11bi/D2.1.

Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/config.c
@@ -666,6 +666,42 @@ static char * wpa_config_write_psk(const struct parse_data *data,
 #endif /* NO_CONFIG_WRITE */
 
 
+static int wpa_config_parse_alt_sae_password_ids(const struct parse_data *data,
+						 struct wpa_ssid *ssid,
+						 int line, const char *value)
+{
+	struct wpabuf *tmp;
+
+	if (!value[0]) {
+		wpabuf_array_free(ssid->alt_sae_password_ids);
+		return 0;
+	}
+
+	tmp = wpabuf_parse_bin(value);
+	if (!tmp) {
+		wpa_printf(MSG_ERROR, "Line %d: invalid alt_sae_password_ids",
+			   line);
+		return -1;
+	}
+
+	if (!ssid->alt_sae_password_ids)
+		ssid->alt_sae_password_ids = wpabuf_array_alloc();
+	if (wpabuf_array_add(ssid->alt_sae_password_ids, tmp) < 0)
+		return -1;
+	return 0;
+}
+
+
+#ifndef NO_CONFIG_WRITE
+static char *
+wpa_config_write_alt_sae_password_ids(const struct parse_data *data,
+				      struct wpa_ssid *ssid)
+{
+	return NULL;
+}
+#endif /* NO_CONFIG_WRITE */
+
+
 static int wpa_config_parse_proto(const struct parse_data *data,
 				  struct wpa_ssid *ssid, int line,
 				  const char *value)
@@ -2554,6 +2590,7 @@ static const struct parse_data ssid_fields[] = {
 	{ INT(mem_only_psk) },
 	{ STR_KEY(sae_password) },
 	{ STR(sae_password_id) },
+	{ FUNC(alt_sae_password_ids) },
 	{ INT(sae_pwe) },
 	{ FUNC(proto) },
 	{ FUNC(key_mgmt) },
@@ -2786,6 +2823,7 @@ static const struct parse_data ssid_fields[] = {
 	{ INT_RANGE(max_idle, 0, 65535)},
 	{ INT_RANGE(ssid_protection, 0, 1)},
 	{ INT_RANGE(rsn_overriding, 0, 2)},
+	{ INT_RANGE(sae_password_id_change, 0, 1)},
 };
 
 #undef OFFSET
@@ -2958,6 +2996,7 @@ void wpa_config_free_ssid(struct wpa_ssid *ssid)
 	os_free(ssid->ext_psk);
 	str_clear_free(ssid->sae_password);
 	os_free(ssid->sae_password_id);
+	wpabuf_array_free(ssid->alt_sae_password_ids);
 #ifdef IEEE8021X_EAPOL
 	eap_peer_config_free(&ssid->eap);
 #endif /* IEEE8021X_EAPOL */

src/common/sae.c
@@ -1065,6 +1065,12 @@ sae_derive_pt_group(int group, const u8 *ssid, size_t ssid_len,
 	if (!pt)
 		return NULL;
 
+	if (identifier) {
+		pt->password_id = wpabuf_alloc_copy(identifier, identifier_len);
+		if (!pt->password_id)
+			goto fail;
+	}
+
 #ifdef CONFIG_SAE_PK
 	os_memcpy(pt->ssid, ssid, ssid_len);
 	pt->ssid_len = ssid_len;
@@ -1281,6 +1287,7 @@ void sae_deinit_pt(struct sae_pt *pt)
 		crypto_ec_point_deinit(pt->ecc_pt, 1);
 		crypto_bignum_deinit(pt->ffc_pt, 1);
 		crypto_ec_deinit(pt->ec);
+		wpabuf_free(pt->password_id);
 		prev = pt;
 		pt = pt->next;
 		os_free(prev);

src/common/sae.h
@@ -103,6 +103,7 @@ struct sae_pt {
 
 	const struct dh_group *dh;
 	struct crypto_bignum *ffc_pt;
+	struct wpabuf *password_id;
 #ifdef CONFIG_SAE_PK
 	u8 ssid[32];
 	size_t ssid_len;

src/common/wpa_common.c
@@ -3523,6 +3523,15 @@ static int wpa_parse_generic(const u8 *pos, struct wpa_eapol_ie_parse *ie)
 		return 0;
 	}
 
+	if (left > 2 && selector == RSN_KEY_DATA_SAE_PW_IDS) {
+		ie->sae_pw_ids = p;
+		ie->sae_pw_ids_len = left;
+		wpa_hexdump_key(MSG_DEBUG,
+				"RSN: SAE Password Identifiers in EAPOL-Key",
+				pos, dlen);
+		return 0;
+	}
+
 	if (left >= 1 && selector == WFA_KEY_DATA_IP_ADDR_REQ) {
 		ie->ip_addr_req = p;
 		wpa_hexdump(MSG_DEBUG, "WPA: IP Address Request in EAPOL-Key",

src/common/wpa_common.h
@@ -673,6 +673,8 @@ struct wpa_eapol_ie_parse {
 	size_t igtk_len;
 	const u8 *bigtk;
 	size_t bigtk_len;
+	const u8 *sae_pw_ids;
+	size_t sae_pw_ids_len;
 	const u8 *mdie;
 	size_t mdie_len;
 	const u8 *ftie;

src/rsn_supp/wpa.c
@@ -2717,6 +2717,52 @@ failed:
 }
 
 
+static void wpa_sm_sae_pw_id_change(struct wpa_sm *sm, const u8 *kde,
+				    size_t kde_len)
+{
+	const u8 *pos = kde, *end = kde + kde_len;
+	u8 flags;
+	struct wpabuf_array *wa;
+
+	wpa_hexdump(MSG_DEBUG, "RSN: Received SAE Password Identifiers KDE",
+		    kde, kde_len);
+
+	if (end - pos < 1)
+		return;
+	flags = *pos++;
+	wpa_printf(MSG_DEBUG, "RSN: SAE Password Identifiers Flags: 0x%x",
+		   flags);
+
+	if (!sm->ctx->sae_pw_id_change)
+		return;
+
+	wa = wpabuf_array_alloc();
+	if (!wa)
+		return;
+
+	while (end > pos) {
+		u8 len;
+
+		len = *pos++;
+		if (end - pos < len) {
+			wpa_printf(MSG_INFO,
+				   "RSN: Truncated SAE Password Identifier Tuple");
+			wpabuf_array_free(wa);
+			return;
+		}
+		wpa_hexdump(MSG_DEBUG, "RSN: SAE Password Identifier",
+			    pos, len);
+		if (wpabuf_array_add(wa, wpabuf_alloc_copy(pos, len))) {
+			wpabuf_array_free(wa);
+			return;
+		}
+		pos += len;
+	}
+
+	sm->ctx->sae_pw_id_change(sm->ctx->ctx, wa);
+}
+
+
 static void wpa_supplicant_process_3_of_4(struct wpa_sm *sm,
 					  const struct wpa_eapol_key *key,
 					  u16 ver, const u8 *key_data,
@@ -2997,6 +3043,9 @@ static void wpa_supplicant_process_3_of_4(struct wpa_sm *sm,
 
 	if (ie.transition_disable)
 		wpa_sm_transition_disable(sm, ie.transition_disable[0]);
+	if (ie.sae_pw_ids && wpa_key_mgmt_sae(sm->key_mgmt) &&
+	    sm->sae_pw_id_change)
+		wpa_sm_sae_pw_id_change(sm, ie.sae_pw_ids, ie.sae_pw_ids_len);
 	sm->msg_3_of_4_ok = 1;
 	return;
 
@@ -5070,6 +5119,9 @@ int wpa_sm_set_param(struct wpa_sm *sm, enum wpa_sm_conf_params param,
 		break;
 	case WPA_PARAM_RSN_OVERRIDE_SUPPORT:
 		sm->rsn_override_support = value;
+		break;
+	case WPA_PARAM_SAE_PW_ID_CHANGE:
+		sm->sae_pw_id_change = !!value;
 		break;
 	default:
 		break;

src/rsn_supp/wpa.h
@@ -105,6 +105,7 @@ struct wpa_sm_ctx {
 	void (*notify_pmksa_cache_entry)(void *ctx,
 					 struct rsn_pmksa_cache_entry *entry);
 	void (*ssid_verified)(void *ctx);
+	void (*sae_pw_id_change)(void *ctx, struct wpabuf_array *wa);
 };
 
 
@@ -143,6 +144,7 @@ enum wpa_sm_conf_params {
 	WPA_PARAM_SPP_AMSDU,
 	WPA_PARAM_URNM_MFPR,
 	WPA_PARAM_URNM_MFPR_X20,
+	WPA_PARAM_SAE_PW_ID_CHANGE,
 };
 
 enum wpa_rsn_override {

src/rsn_supp/wpa_i.h
@@ -115,6 +115,7 @@ struct wpa_sm {
 	unsigned int prot_range_neg_x20:1;
 	unsigned int ssid_protection:1;
 	unsigned int spp_amsdu:1;
+	unsigned int sae_pw_id_change:1;
 
 	u8 *assoc_wpa_ie; /* Own WPA/RSN IE from (Re)AssocReq */
 	size_t assoc_wpa_ie_len;

sailfishos-mirror/hostap

commit 4691e72a05588fad3306c75883e21b78848fe6b8

SAE: Fix clearing of alt_sae_password_ids config parameter
The parser did not clear the ssid->alt_sae_password_ids pointer when
freeing the array of password identifiers. This could result in
double-freeing the memory if attempting to clear that list of
alternative SAE password identifiers through the control interface.

Fix this by explicitly clearing ssid->alt_sae_password_ids to NULL when
freeing the array.

Credit to NVIDIA Project Vanessa for discovering and reporting this
issue.

Fixes: ef228583e331 ("SAE: Password identifier changing (STA)")
Signed-off-by: Jouni Malinen <j@w1.fi>

wpa_supplicant/config.c
@@ -737,6 +737,7 @@ static int wpa_config_parse_alt_sae_password_ids(const struct parse_data *data,
 
 	if (!value[0]) {
 		wpabuf_array_free(ssid->alt_sae_password_ids);
+		ssid->alt_sae_password_ids = NULL;
 		return 0;
 	}
 