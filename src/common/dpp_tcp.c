vanhoefm/fragattacks

commit fc3efe0834e64eafca38236700111e0f9ab32329

DPP2: Support mutual auth with QR in scan-during-auth-exchange case
Extend DPP authentication session search for the DPP_QR_CODE command to
cover the ongoing exchanges in Controller/Responder.

Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

src/common/dpp_tcp.c
@@ -1719,6 +1719,28 @@ struct dpp_authentication * dpp_controller_get_auth(struct dpp_global *dpp,
 }
 
 
+void dpp_controller_new_qr_code(struct dpp_global *dpp,
+				struct dpp_bootstrap_info *bi)
+{
+	struct dpp_controller *ctrl = dpp->controller;
+	struct dpp_connection *conn;
+
+	if (!ctrl)
+		return;
+
+	dl_list_for_each(conn, &ctrl->conn, struct dpp_connection, list) {
+		struct dpp_authentication *auth = conn->auth;
+
+		if (!auth->response_pending ||
+		    dpp_notify_new_qr_code(auth, bi) != 1)
+			continue;
+		wpa_printf(MSG_DEBUG,
+			   "DPP: Sending out pending authentication response");
+		dpp_tcp_send_msg(conn, conn->auth->resp_msg);
+	}
+}
+
+
 void dpp_tcp_init_flush(struct dpp_global *dpp)
 {
 	struct dpp_connection *conn, *tmp;

src/common/dpp.h
@@ -668,6 +668,8 @@ int dpp_controller_start(struct dpp_global *dpp,
 void dpp_controller_stop(struct dpp_global *dpp);
 struct dpp_authentication * dpp_controller_get_auth(struct dpp_global *dpp,
 						    unsigned int id);
+void dpp_controller_new_qr_code(struct dpp_global *dpp,
+				struct dpp_bootstrap_info *bi);
 int dpp_tcp_init(struct dpp_global *dpp, struct dpp_authentication *auth,
 		 const struct hostapd_ip_addr *addr, int port,
 		 const char *name);

wpa_supplicant/dpp_supplicant.c
@@ -90,6 +90,10 @@ int wpas_dpp_qr_code(struct wpa_supplicant *wpa_s, const char *cmd)
 				       500, wpas_dpp_tx_status, 0);
 	}
 
+#ifdef CONFIG_DPP2
+	dpp_controller_new_qr_code(wpa_s->dpp, bi);
+#endif /* CONFIG_DPP2 */
+
 	return bi->id;
 }
 

sailfishos-mirror/hostap

commit 79eef8837bd5523a4094fbc476c02581cf9f3d00

DPP2: Fix QR code addition in Controller for not yet started TCP connections
The TCP connections maintained by the Controller may include instances
that have not yet fully started the authentication exchange. conn->auth
is NULL for such cases and the iteration of updating all existing
connections with a new QR Code (e.g., the DPP_QR_CODE control interface
command) would result in process termination due to dereferencing a NULL
pointer.

Credit to NVIDIA Project Vanessa for discovering and reporting this
issue.

Fixes: fc3efe0834e6 ("DPP2: Support mutual auth with QR in scan-during-auth-exchange case")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/common/dpp_tcp.c
@@ -2291,7 +2291,7 @@ void dpp_controller_new_qr_code(struct dpp_global *dpp,
 	dl_list_for_each(conn, &ctrl->conn, struct dpp_connection, list) {
 		struct dpp_authentication *auth = conn->auth;
 
-		if (!auth->response_pending ||
+		if (!auth || !auth->response_pending ||
 		    dpp_notify_new_qr_code(auth, bi) != 1)
 			continue;
 		wpa_printf(MSG_DEBUG,